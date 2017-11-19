3d2512eb147294cbb00e0efe6c249349727cbf2c
Chet <chethana96@gmail.com>
2017-11-19 23:09:10 +1300

    completed carousel and working on other main page components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'antd/dist/antd.min.css';
-import logo from './img/logo1.png';
+import logo from './img/logo2.png';
+import phones from './img/banner-iphone-samsung.png';
+import cables from './img/banner-cables.png';
+import oeh from './img/banner-oeh.png';
+import apple from './img/showcase-apple.png';
+import samsung from './img/showcase-samsung.png';
+import huawei from './img/showcase-huawei.png';
+
 
 import { Layout, Menu, Icon, Input, Avatar, Button, Carousel, Row, Col } from 'antd';
 const { Header, Content, Footer } = Layout;
@@ -13,7 +20,7 @@ class App extends Component {
   render() {
     return (
       <Layout className="layout">
-        <Header style={{ position: "fixed", zIndex: 3, width: "100%" }}>
+        <Header style={{ position: "fixed", zIndex: 3, width: "100%", boxShadow: "0px 2px 20px #404040" }}>
           <div className="logo">
             <img src={logo} alt="Logo for Principle" />
           </div>
@@ -22,7 +29,7 @@ class App extends Component {
             mode="horizontal"
             theme="light"
             className="menu"
-            style={{ width: "300px", background: "#404040", color: "#ffffff", fontSize: "10pt", top: "-50px", left: "210px", borderBottom: "#404040", position: "relative", }}
+            style={{ width: "300px", color: "#ffffff", fontSize: "10pt", top: "-50px", left: "210px", borderBottom: "#404040", position: "relative", color: "#404040" }}
           >
             <Menu.Item key="home" style={{ padding: "0 25px", top: "-5px" }}>
               Home
@@ -36,7 +43,7 @@ class App extends Component {
           </Menu>
           <Search
             placeholder="Search"
-            style={{ width: 200, position: "relative", left: "500px", top: "-115px" }}
+            style={{ width: 200, position: "relative", left: "510px", top: "-115px" }}
             onSearch={value => console.log(value)}
           />
           <Avatar size="large" icon="user"
@@ -46,26 +53,110 @@ class App extends Component {
             Log In</Button>
 
         </Header>
-        <Carousel autoplay="true">
-          <div><h3>1</h3></div>
-          <div><h3>2</h3></div>
-          <div><h3>3</h3></div>
-          <div><h3>4</h3></div>
-        </Carousel>
-        <Content style={{ padding: '10px 50px' }}>
-          <Row>
-            <Col style={{border: "solid"}} xs={{span: 24}}>
-              <h1>What We Do</h1>
-            </Col>
-            
-          </Row>
+        <Carousel >
+          <div>
+            <div style={{ height: "500px" }}>
+              <img src={phones} style={{ height: "500px", marginLeft: "50px" }} />
+              <h1 style={{ lineHeight: "20px", position: "relative", left: "15%", top: "-250px" }}>Premium Smartphones</h1>
+              <h2 style={{ lineHeight: "20px", position: "relative", left: "15%", top: "-250px" }}>Everyday</h2>
 
+            </div>
+          </div>
+          <div>
+            <div style={{ height: "500px", background: "#000000" }}>
+              <img src={oeh} style={{ height: "500px", position: "relative", left: "25%" }} />
+              <h1 style={{ color: "#ffffff", lineHeight: "20px", position: "relative", top: "-250px", left: "-250px" }}>Block it Out.</h1>
+              <h2 style={{ color: "#ffffff", lineHeight: "20px", position: "relative", top: "-250px", left: "-250px" }}>Noise Cancelling Headphones</h2>
+            </div>
+          </div>
+          <div>
+            <div style={{ height: "500px" }}>
+              <img src={cables} style={{ height: "250px", position: "relative", left: "20%", top: "50%" }} />
+              <h1 style={{ lineHeight: "20px", position: "relative", top: "-100px" }}>Color-Matched Cables</h1>
+              <h2 style={{ lineHeight: "20px", position: "relative", top: "-100px" }}>Strong. Affordable. Yours.</h2>
+            </div>
+          </div>
+        </Carousel>
+        <Content style={{ padding: '20px 50px', background: "#ffffff" }}>
+          <Row gutter={16}>
+            <Col xs={{ span: 24 }} style={{ paddingBottom: "20px", textAlign: "center" }}>
+              <h1>Shop Phones by Brand</h1>
+            </Col>
+          </Row>
+          <Row gutter={16} className="showcase-row">
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={apple} className="showcase-img" />
+              </div>
+            </Col>
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={samsung} className="showcase-img" />
+              </div>
+            </Col>
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={huawei} className="showcase-img" />
+              </div>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col xs={{ span: 24 }} style={{ paddingBottom: "20px", textAlign: "center" }}>
+              <h1>Shop Headphones by Brand</h1>
+            </Col>
+          </Row>
+          <Row gutter={16}  className="showcase-row">
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={apple} className="showcase-img" />
+              </div>
+            </Col>
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={samsung} className="showcase-img" />
+              </div>
+            </Col>
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={huawei} className="showcase-img" />
+              </div>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+              <Col xs={{ span: 24 }} style={{ paddingBottom: "20px", textAlign: "center" }}>
+                <h1>Shop Cables by Type</h1>
+              </Col>
+          </Row>
+          <Row gutter={16}  className="showcase-row" >
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={apple} className="showcase-img" />
+              </div>
+            </Col>
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={samsung} className="showcase-img" />
+              </div>
+            </Col>
+            <Col xs={{ span: 8 }}>
+              <div className="showcase-block">
+                <img src={huawei} className="showcase-img" />
+              </div>
+            </Col>
+          </Row>          
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Chethana Wijesekera
+        <Row style={{background: "#fff" ,textAlign: "center"}} className="bottom-search" >
+            <Col span={24}>
+              <h1 style={{position: "relative", top: "50px"}}>Or Search the Store</h1>
+              <Search style={{ width: "50%", margin: "8% 20%"}} placeholder="Search" onSearch={value => console.log(value)} />
+              <Button style={{position: "relative", left: "-19%" }}type="primary" icon="search">Search</Button>
+            </Col>
+          </Row>
+          <Footer style={{ textAlign: 'center' }}>
+            Chethana Wijesekera
         </Footer>
       </Layout>
-    );
+        );
   }
 }
 
